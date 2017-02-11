977e080df2a2596a2d463a90236a653cf4ce2322
nerkar <nerkarpraveen@gmail.com>
2017-02-11 13:57:07 +0530

    added modified

--- api-integration/index.js
+++ api-integration/index.js
@@ -1,18 +1,24 @@
-
-var key = "29b005cdbc47b710fae6e0859c0e35e73cb716702abe9336beee2a7cc847842b";
-var secret = "6350d693f3c4938628bc5d199a900102e11fd88fb99aeeee536b91fe07e3367f";
+// API Token key and secret are available from control panel when API token is generated
+const key = 'dacaffe7ce69dfd1071531e925f667905a1c981fb40d06c676880e84352cb3aa';
+const secret = '5b70319201e9abad12a3458b32ed30cf634ef569ea47906e5012baf11cab5046';
 
 // Hash time and key with secret
-var hash = "";
-var epoch = (int)(DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalSeconds;
-using(var hmac = new HMACSHA256(Encoding.ASCII.GetBytes(secret))) {
-  var bytes = hmac.ComputeHash(Encoding.ASCII.GetBytes(key + epoch));
-  hash = BitConverter.ToString(hash).Replace("-", "");
-}
-var token = string.Format("{0}_{1}_{2}", key, epoch, hash);
+const crypto = require('crypto');
+const hmac = crypto.createHmac('sha256', secret);
+const epoch = Math.floor(Date.now() / 1000);
+hmac.update(`${key}${epoch}`);
+const hash = hmac.digest('hex');
+const token = `${key}_${epoch}_${hash}`;
 
-// send token as X-Deki-Client HTTP header to MindTouch API (WebRequest is used in this example)
-var request = WebRequest.Create('https://success.example.com/@api/deki/pages/home/info');
-request.Method = "GET";
-request.Headers.Add('X-Deki-Client', token);
-var response = request.GetResponse();
+// send token as X-Deki-Client HTTP header to MindTouch API (https://github.com/request/request is used in this example)
+const request = require('request');
+request({
+  url: 'https://success.example.com/@api/deki/pages/home/info',
+  headers: {
+    'X-Deki-Token': token
+  }
+}, (error, response, body) => {
+
+  // ...
+});
+
